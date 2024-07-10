0db395b139edc867c6f52df42106fcba279352a2
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2024-07-10 09:29:18 +0300

    last commit isa

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import { config } from "dotenv";
 import express from "express";
 import app from "./src/app.js";
-config()
-const port = process.env.PORT
+config();
+const port = process.env.PORT;
+app.get("/", async (req, res, next) => {
+  return res.json({
+    status: 200,
+    message: "Welcome in Ekhtisaas ..!",
+  });
+});
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-})
+  console.log(`server running on port ${port}`);
+});
