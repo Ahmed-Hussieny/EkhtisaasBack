8d4099b6b115a48b39863e5c346b523c86844fff
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2024-07-11 19:38:35 +0300

    last commit

--- src/middleWares/multer.js
+++ src/middleWares/multer.js
@@ -1,24 +1,60 @@
 import multer from "multer";
 
 import { allowedExtensions } from "../utils/allowedExtensions.js";
+import generateUniqueString from "../utils/generateUniqueString.js";
+import path from 'path' 
+import fs from 'fs'
+// export const mullterMiddleHost = ({ extension = allowedExtensions.image }) => {
+//   const storage = multer.diskStorage({});
 
-export const mullterMiddleHost = ({ extension = allowedExtensions.image }) => {
-  const storage = multer.diskStorage({});
+//   const fileFilter = (req, file, cb) => {
+//     // Extract the file extension
+//     const fileExtension = file.originalname.split('.').pop().toLowerCase();
 
-  const fileFilter = (req, file, cb) => {
-    // Extract the file extension
-    const fileExtension = file.originalname.split('.').pop().toLowerCase();
-
-    // Check if the file extension is allowed
-    if (extension.includes(fileExtension)) {
-      // Allow the file
-      return cb(null, true);
-    } else {
-      // Reject the file with an error message
-      return cb(new Error("Invalid file extension"));
-    }
+//     // Check if the file extension is allowed
+//     if (extension.includes(fileExtension)) {
+//       // Allow the file
+//       return cb(null, true);
+//     } else {
+//       // Reject the file with an error message
+//       return cb(new Error("Invalid file extension"));
+//     }
     
-  };
-  const file = multer({ fileFilter, storage });
-  return file;
-};
+//   };
+//   const file = multer({ fileFilter, storage });
+//   return file;
+// };
+export const mullterMiddleHost = ({
+  extensions = allowedExtensions.image,
+  filePath = 'general'
+}) => {
+
+  const destinationPath = path.resolve(`../Images/uploads/${filePath}`) // return the full path till the src/uploads/${filePath}
+
+  // path check
+  if (!fs.existsSync(destinationPath)) {
+      fs.mkdirSync(destinationPath, { recursive: true })
+  }
+  // diskStorage
+  const storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+          cb(null, destinationPath)
+      },
+      filename: (req, file, cb) => {
+          const uniqueFileName = generateUniqueString(6) + '_' + file.originalname
+          cb(null, uniqueFileName)
+      }
+  })
+
+  // file Filter
+  const fileFilter = (req, file, cb) => {
+      if (extensions.includes(file.mimetype.split('/')[1])) {
+          return cb(null, true)
+      }
+      cb(new Error('Image format is not allowed!'), false)
+  }
+
+
+  const file = multer({ fileFilter, storage })
+  return file
+}
