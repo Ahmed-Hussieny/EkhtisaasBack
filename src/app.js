481f148d8a0f5192f9c9e0175768219063f973d0
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2024-07-12 23:46:57 +0300

    last commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
-// src/app.js
-
 import { config } from "dotenv";
 import express from "express";
-import cors from "cors";
-import fs from 'fs';
-import https from 'https';
+import cors from "cors"; 
 import db_Connection from "../DB/connection.js";
 import UserRoute from "./modules/User/user.routes.js";
 import { globalResponse } from "./middleWares/globalResponce.js";
@@ -16,31 +12,25 @@ import selfEducationRouter from "./modules/selfEducation/selfEducation.routes.js
 import supportSideRouter from "./modules/supportSide/supportSide.routes.js";
 import AdvisorRouter from "./modules/Advisor/Advisor.routes.js";
 import SpecialistRouter from "./modules/Specialist/Specialist.routes.js";
-
 const app = express();
-const corsOptions = {
-  origin: ['https://ekhtisaas.com', 'http://localhost:3000','https://webeu.info','http://ekhtisaas.com','http://localhost:3001'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204
-};
+app.use(cors({ origin: ['http://localhost:3001','https://webeu.info','http://localhost:3000','https://ekhtisaas.com','http://ekhtisaas.com'] }));
 
-app.use(cors(corsOptions));
-app.use(express.json());
-
+app.use(express.json())
 config();
-db_Connection();
+db_Connection()
+app.use('/user', UserRoute)
+app.use('/MainSpecialty', MainSpecialtyRouter)
+app.use('/SubSpecialty', SubSpecialtyRouter)
+app.use('/Certificate',CertificateRouter)
+app.use('/directEducation',directEducationRouter)
+app.use('/selfEducation',selfEducationRouter)
+app.use('/supportSide',supportSideRouter)
+app.use('/Advisor',AdvisorRouter)
+app.use('/Specialist',SpecialistRouter)
 
-app.use('/user', UserRoute);
-app.use('/MainSpecialty', MainSpecialtyRouter);
-app.use('/SubSpecialty', SubSpecialtyRouter);
-app.use('/Certificate', CertificateRouter);
-app.use('/directEducation', directEducationRouter);
-app.use('/selfEducation', selfEducationRouter);
-app.use('/supportSide', supportSideRouter);
-app.use('/Advisor', AdvisorRouter);
-app.use('/Specialist', SpecialistRouter);
 
-app.use(globalResponse);
+
+
+app.use(globalResponse)
 
 export default app;
