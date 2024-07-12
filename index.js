0cfc0a841bba95a5fe1938ad51f8e42d6d5a6146
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2024-07-12 23:23:06 +0300

    last commit

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 import { config } from "dotenv";
-import fs from 'fs';
-import https from 'https';
+import express from "express";
 import app from "./src/app.js";
-
 config();
-
-const port = 443; // Change to port 443
-
-// SSL Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/ekhtisaas.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/ekhtisaas.com/fullchain.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/ekhtisaas.com/fullchain.pem', 'utf8');
-
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca
-};
-
+const port = process.env.PORT;
 app.get("/", async (req, res, next) => {
   return res.json({
     status: 200,
-    message: "Welcome to Ekhtisaas ..!",
+    message: "Welcome in Ekhtisaas ..!",
   });
 });
-
-https.createServer(credentials, app).listen(port, () => {
-  console.log(`Server running on port ${port} with HTTPS`);
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
