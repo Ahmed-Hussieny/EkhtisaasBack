3537f58a273b6f03c7ca59b36da0f4073eee8df1
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2024-07-12 10:17:32 +0300

    last commit

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 import { config } from "dotenv";
 import express from "express";
+import https from "https";
+import http from "http";
+import fs from "fs";
 import app from "./src/app.js";
+
 config();
-const port = process.env.PORT;
-app.get("/", async (req, res, next) => {
-  return res.json({
-    status: 200,
-    message: "Welcome in Ekhtisaas ..!",
-  });
+
+const httpsPort = process.env.HTTPS_PORT || 443; // HTTPS port
+const httpPort = process.env.HTTP_PORT || 80; // HTTP port
+
+// HTTPS options: You need to provide your SSL certificate and private key
+const httpsOptions = {
+  key: fs.readFileSync("./private.key"),
+  cert: fs.readFileSync("./certificate.crt"),
+};
+
+// Create HTTPS server
+const httpsServer = https.createServer(httpsOptions, app);
+
+httpsServer.listen(httpsPort, () => {
+  console.log(`HTTPS Server running on port ${httpsPort}`);
 });
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+
+// Create HTTP server to redirect to HTTPS
+const httpApp = express();
+
+httpApp.get("*", (req, res) => {
+  res.redirect(`https://${req.headers.host}${req.url}`);
+});
+
+const httpServer = http.createServer(httpApp);
+
+httpServer.listen(httpPort, () => {
+  console.log(`HTTP Server running on port ${httpPort}`);
 });
