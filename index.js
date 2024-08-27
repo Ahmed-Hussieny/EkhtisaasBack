c346d59242434273d2d90d594e36f18331b6ce0b
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2024-08-27 22:16:48 +0300

    deploy on render

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ config();
 const port = process.env.PORT || 3000;
 
 // // Path to your SSL certificate and key
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/srv548846.hstgr.cloud/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/srv548846.hstgr.cloud/fullchain.pem')
-};
+// const options = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/srv548846.hstgr.cloud/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/srv548846.hstgr.cloud/fullchain.pem')
+// };
 
 app.get("/", async (req, res, next) => {
   return res.json({
@@ -19,11 +19,11 @@ app.get("/", async (req, res, next) => {
   });
 });
 
-https.createServer(options, app).listen(port, "0.0.0.0" ,() => {
-  console.log(`HTTPS server running on port ${port}`);
-});
+// https.createServer(options, app).listen(port, "0.0.0.0" ,() => {
+//   console.log(`HTTPS server running on port ${port}`);
+// });
 
-// app.listen(port, "0.0.0.0" ,() => {
-//     console.log(`HTTPS server running on port ${port}`);
-//   });
+app.listen(port, "0.0.0.0" ,() => {
+    console.log(`HTTPS server running on port ${port}`);
+  });
   
